73c121ebd991ad02274b004d697622001b85888d
Scott Irwin <sjirwin@users.noreply.github.com>
2026-03-12 20:33:17 +0000

    add each step of the worked Point example

---
+++ 04/point.py
@@ -0,0 +1,28 @@
+import math
+
+from descriptor import CachedProperty
+
+
+class Point:
+    def __init__(self, x, y):
+        self.x = x
+        self.y = y
+
+    @CachedProperty
+    def r(self):
+        print("Computing r ...")
+        return math.sqrt(self.x**2 + self.y**2)
+
+    @CachedProperty
+    def theta(self):
+        print("Computing theta ...")
+        return math.atan2(self.y, self.x)
+
+
+if __name__ == "__main__":
+    p = Point(3, 4)
+
+    print(f"First access to r={p.r}")  # Calculates: 5.0
+    print(f"Second access to r={p.r}")  # Cached: 5.0
+    p.x = 5
+    print(f"After changing x, r={p.r}")  # Invalid cached value
